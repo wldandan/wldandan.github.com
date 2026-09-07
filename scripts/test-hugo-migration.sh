0ae32ff2adc132476d6bc51d925de6c56d2b916e
leon-wang2021 <5109343@qq.com>
2026-09-07 15:54:17 +0800

    Render content on page and archive listings

--- scripts/test-hugo-migration.sh
+++ scripts/test-hugo-migration.sh
@@ -13,6 +13,7 @@ test -f layouts/_default/single.html || fail "missing single template"
 test -f layouts/partials/header.html || fail "missing header partial"
 test -f static/editorial.css || fail "missing editorial stylesheet"
 test -f static/stylesheets/screen.css || fail "missing base stylesheet"
+grep -q '\.Content' layouts/_default/list.html || fail "list template does not render page content"
 test -f .github/workflows/hugo-pages.yml || fail "missing GitHub Pages workflow"
 
 hugo_posts=$(find content/posts -type f -name '*.md' | wc -l | tr -d ' ')
